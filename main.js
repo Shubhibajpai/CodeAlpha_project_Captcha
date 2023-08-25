58cfee80b6f258d648b9ad3011dbcf8616674c7c
shubhi bajpai <118922207+Shubhibajpai@users.noreply.github.com>
2023-08-25 14:21:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function generateCaptcha() {
+    var captchaText = generateRandomString(6);
+    document.getElementById('captcha-text').textContent = captchaText;
+}
+
+function generateRandomString(length) {
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result= '';
+    for(var i = 0; i<length; i++) {
+        var randomIndex = Math.floor(Math.random()*characters.length);
+        result += characters.charAt(randomIndex);
+    }
+return result;    
+}
+function verifyCaptcha() {
+    var userEnteredText = document.getElementById('user-input').value;
+    var captchaText = document.getElementById('captcha-text').textContent;
+    var resultMessage = document.getElementById('result-message');
+
+    if(userEnteredText === captchaText) {
+        resultMessage.textContent= 'CAPTCHA verified! you are human.';
+        resultMessage.style.color = 'green';
+    }else{
+        resultMessage.textContent = 'CAPTCHA verification failed. Please try again.';
+        resultMessage.style.color = 'red';
+    }
+}
